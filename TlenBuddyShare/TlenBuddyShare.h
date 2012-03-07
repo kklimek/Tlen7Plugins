f696f90b61feaa539504b9d5b7d7507141a9f73e
Kamil Klimek <naresh@tlen.pl>
2012-03-07 22:00:44 +0100

    friendlyName i opis

--- TlenBuddyShare/TlenBuddyShare.h
+++ TlenBuddyShare/TlenBuddyShare.h
@@ -8,13 +8,16 @@ class TlenBuddyShare: public TlenPlugin
 {
 public:
 	QString name() const { return QString::fromLatin1("TlenBuddyShare"); }
+
+	QString friendlyName() const { return QObject::tr("Share buddies"); }
+
 	QString author() const { return AUTHOR; }
 	QString web() const { return WEB; }
 	QString email() const { return EMAIL; }
 
 	QString icon(int) const { return QString(); }
 
-	QString description() const { return QObject::tr("Easily export and import buddies. Uses Tlen 6 contact list xml format."); }
+	QString description() const { return QObject::tr("Send your buddies to your buddies. Easily send your contact list to your friends. Also supports import and export to Tlen 6 XML format file."); }
 
 	int version() const { return TLEN_PLUGIN_VERSION(0,0,1); }
 
